507292d8a53e0c03ee30eb0806ed5d4d3e2de618
Dave Atkinson <davidatkinson@xmos.com>
2021-10-08 10:42:28 +0100

    Add copyright.

--- xmos-cpm.cmake
+++ xmos-cpm.cmake
@@ -1,4 +1,5 @@
-
+# Copyright 2015-2021 XMOS LIMITED.
+# This Software is subject to the terms of the XMOS Public Licence: Version 1.
 macro(xmos_add_package NAME TAG)
   CPMAddPackage(
     NAME "${NAME}"
